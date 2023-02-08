3483ea32060552fbec0332e8e6e83115831ea30d
annafu1 <104175541+annafu1@users.noreply.github.com>
2023-02-08 16:54:59 -0500

    total population found in Asia

---
+++ Problems/TotalPopulation.sql
@@ -0,0 +1,4 @@
+--Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+SELECT SUM(CITY.Population) FROM CITY
+INNER JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
+WHERE CONTINENT = 'Asia';
