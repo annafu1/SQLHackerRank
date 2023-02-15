5097493b5978851bb4cdb14306c9677b0242a40d
Anna Huang <annahuang@Annas-MBP.fios-router.home>
2023-02-15 12:23:01 -0500

    updated

--- Problems/AveragePopulation.sql
+++ Problems/AveragePopulation.sql
@@ -1,4 +1,4 @@
 --Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
 SELECT COUNTRY.Continent, FLOOR(AVG(City.Population)) FROM City
 INNER JOIN COUNTRY on CITY.CountryCode = COUNTRY.Code
-GROUP BY COUNTRY.Continent;
+GROUP BY COUNTRY.Continent
